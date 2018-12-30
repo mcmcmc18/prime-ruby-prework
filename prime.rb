d0ccb56c427977f9f2078737b8499d92ee8408c4
MC Simoes <mariacristina.simoes@gmail.com>
2018-12-30 21:22:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,14 @@
 # Add  code here!
 
 def prime?(n)
-
+range_of_nums = (2..(n-1)).to_a
+if n < 2 
+  return FALSE
+else
+  range.each do |x|
+    if n % x == 0 
+      return FALSE
+end
+end
  # if (n % 2 == 0 && n / 2 == 0 && n > 1 ) || (n % 3 == 0 && n / 3 == 0  && n > 1)
 
