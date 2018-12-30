8ab58fabef4a8afbf54ffcb24e4320ebbbdd96d3
MC Simoes <mariacristina.simoes@gmail.com>
2018-12-30 21:10:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 
 def prime?(n)
-  if (n % 2 == 0 && n / 2 == 0) || (n % 3 == 0 && n / 3 == 0 )
+  if (n % 2 == 0 && n / 2 == 0 && n > 1 ) || (n % 3 == 0 && n / 3 == 0  && n > 1)
     return FALSE
   else 
     return TRUE
