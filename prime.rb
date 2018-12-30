24c004233cd7cbe2f4872b752ac39b1339d91aac
MC Simoes <mariacristina.simoes@gmail.com>
2018-12-30 21:42:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,25 +1,21 @@
 # Add  code here!
 
 def prime?(n)
-  range = (2..(n-1)).to_a 
-  (range.any?{|x| n % x == 0 || n == 0 }|| n == 1 || integer == 0 || integer == 1) ? false : true
-
-  
-  
-   test_divisors = (2..(integer - 1)).to_a	   test_divisors = (2..(integer - 1)).to_a
-   if test_divisors.any? {|divisor| integer % divisor == 0} || integer == 0 || integer == 1 	   (test_divisors.any? {|divisor| integer % divisor == 0} || integer == 0 || integer == 1) ? false : true  
-if 
- range_of_nums = (2...n).to_a
-  range_of_nums.each do |x|
-    if n % x == 0
-      return FALSE
-elsif n < 2 
-return FALSE
-else
-  return TRUE
-end
-end
-end
+   if n == 1
+     return true 
+   elsif n < 1
+     return false
+   else
+   for x in 2..(n-1)
+     if (n % x) == 0
+       return false 
+     end
+   end 
+ end
+ 
+    true
+ end
+ 
 
  # if (n % 2 == 0 && n / 2 == 0 && n > 1 ) || (n % 3 == 0 && n / 3 == 0  && n > 1)
 
