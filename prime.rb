01aaeec17706e9fa197ee26f13c56a4033080920
MC Simoes <mariacristina.simoes@gmail.com>
2018-12-30 21:42:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+
+def prime?(n)
+   if n == 1
+     return false 
+   elsif n < 1
+     return false
+   else
+   for x in 2..(n-1)
+     if (n % x) == 0
+       return false 
+     end
+   end 
+ end
+ 
+    true
+ end
+ 
+
+ # if (n % 2 == 0 && n / 2 == 0 && n > 1 ) || (n % 3 == 0 && n / 3 == 0  && n > 1)
+
+
