1b30c03983c94ada4df9cd4f75013318d3d43c05
MC Simoes <mariacristina.simoes@gmail.com>
2018-12-30 21:21:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,6 @@
 # Add  code here!
 
 def prime?(n)
-  
-if n >= 2 
-  (2..num - 1).each  do |x|
-    num % x != 0 
-  end
-else
-  return false
-end
-end
+
  # if (n % 2 == 0 && n / 2 == 0 && n > 1 ) || (n % 3 == 0 && n / 3 == 0  && n > 1)
 
